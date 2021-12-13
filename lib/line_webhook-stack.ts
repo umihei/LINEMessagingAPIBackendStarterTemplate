2d2d9eecff271210e0bf62d7b0424e7eabdd72a7
wakitani <wakitani.kaihei@gmail.com>
2021-12-13 05:44:50 +0000

    add lambda and apigw

--- lib/line_webhook-stack.ts
+++ lib/line_webhook-stack.ts
@@ -1,16 +1,19 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 
 export class LineWebhookStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const lineWebhookFn = new lambdaNodejs.NodejsFunction(this, 'lineWebhookFn', {
+      entry: 'src/lambda/handlers/lineWebhook.ts',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LineWebhookQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new apigw.LambdaRestApi(this, 'lineWebhookEndpoint', {
+      handler: lineWebhookFn
+    });
+
   }
 }
