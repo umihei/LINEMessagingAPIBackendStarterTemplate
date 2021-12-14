9970a85e2da84ec7130244cb653d4a0d6656d2bc
wakitani <wakitani.kaihei@gmail.com>
2021-12-14 02:06:09 +0000

    add line reply function

--- lib/line_webhook-stack.ts
+++ lib/line_webhook-stack.ts
@@ -2,13 +2,21 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 export class LineWebhookStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const ChannelAccessToken = ssm.StringParameter.fromStringParameterName(this, 'ChannelID', 'CHANNEL_ACCESSTOKEN');
+    const ChannelSecret = ssm.StringParameter.fromStringParameterName(this, 'ChannelSecret', 'CHANNEL_SECRET');
+
     const lineWebhookFn = new lambdaNodejs.NodejsFunction(this, 'lineWebhookFn', {
       entry: 'src/lambda/handlers/lineWebhook.ts',
+      environment: {
+        CHANNEL_ACCESSTOKEN: ChannelAccessToken.stringValue,
+        CHANNEL_SECRET: ChannelSecret.stringValue
+      }
     });
 
     new apigw.LambdaRestApi(this, 'lineWebhookEndpoint', {
--- src/lambda/handlers/lineWebhook.ts
+++ src/lambda/handlers/lineWebhook.ts
@@ -1,5 +1,18 @@
-exports.handler = async function (event: any) {
-    console.log("request:", JSON.stringify(event));
+import { Client } from '@line/bot-sdk';
+
+const client = new Client({
+    channelAccessToken: process.env.CHANNEL_ACCESSTOKEN as string,
+    channelSecret: process.env.CHANNEL_SECRET
+});
+
+exports.handler = async function (req: any) {
+    console.log("request:", JSON.stringify(req));
+    const event = JSON.parse(req.body).events[0]
+
+    await client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    });
 
     return {
         statusCode: 200,
