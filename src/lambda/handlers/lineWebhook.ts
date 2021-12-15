054af020ec71eedf3b59be859935db101d8a55fe
wakitani <wakitani.kaihei@gmail.com>
2021-12-15 12:02:59 +0000

    add type check

--- src/lambda/handlers/lineWebhook.ts
+++ src/lambda/handlers/lineWebhook.ts
@@ -1,18 +1,29 @@
-import { Client } from '@line/bot-sdk';
+import { Client, TextEventMessage, MessageEvent, WebhookRequestBody } from '@line/bot-sdk';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { is } from 'typescript-is';
 
 const client = new Client({
-    channelAccessToken: process.env.CHANNEL_ACCESSTOKEN as string,
+    channelAccessToken: process.env.CHANNEL_ACCESSTOKEN!,
     channelSecret: process.env.CHANNEL_SECRET
 });
 
-exports.handler = async function (req: any) {
+exports.handler = async function (req: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     console.log("request:", JSON.stringify(req));
-    const event = JSON.parse(req.body).events[0]
+    const events: WebhookRequestBody = JSON.parse(req.body!)
 
-    await client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: event.message.text
-    });
+    for (let event of events.events) {
+
+        // text messageだけ返信（やまびこ）
+        if (is<MessageEvent>(event)) {
+            if (is<TextEventMessage>(event.message)) {
+                await client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: event.message.text
+                });
+            }
+        }
+
+    }
 
     return {
         statusCode: 200,
