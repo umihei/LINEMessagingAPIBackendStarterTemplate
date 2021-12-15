2b7d98c82f45e1bbc540abc6f650a6df1e93b597
wakitani <wakitani.kaihei@gmail.com>
2021-12-15 13:07:13 +0000

    self typegurd

--- src/lambda/handlers/lineWebhook.ts
+++ src/lambda/handlers/lineWebhook.ts
@@ -6,15 +6,27 @@ const client = new Client({
     channelSecret: process.env.CHANNEL_SECRET
 });
 
+function isMessageEvent(arg: any): arg is MessageEvent {
+    return arg.MessageEvent !== undefined;
+}
+
+function isTextEventMessage(arg: any): arg is TextEventMessage {
+    return arg.TextEventMessage !== undefined;
+}
+
 exports.handler = async function (req: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> {
     console.log("request:", JSON.stringify(req));
     const events: WebhookRequestBody = JSON.parse(req.body!)
 
     for (let event of events.events) {
-        await client.replyMessage((event as MessageEvent).replyToken, {
-            type: 'text',
-            text: ((event as MessageEvent).message as TextEventMessage).text
-        });
+        if (isMessageEvent(event)) {
+            if (isTextEventMessage(event.message)) {
+                await client.replyMessage(event.replyToken, {
+                    type: 'text',
+                    text: event.message.text
+                });
+            }
+        }
     }
 
     return {
